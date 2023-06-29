a1d75ea44f970878d3353ab069b40a9ad4bf4d59
Karmen <karmengeisel@icloud.com>
2023-06-29 15:09:19 -0600

    Initial Commit

---
+++ myNotebooks/myNotebooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  myNotebooksApp.swift
+//  myNotebooks
+//
+//  Created by Scholar on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct myNotebooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
